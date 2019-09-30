a433dd2f1d1f2aa34cd43f77c066022db2a5ba9c
Eliana Arenas <elianaarenasm@gmail.com>
2019-09-30 16:03:34 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,10 +14,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 // ==================================
 let urlDB;
 
-//if(process.env.NODE_ENV === 'dev'){
-    //urlDB = 'mongodb://localhost:27017/cafe';
-//} else{
-    urlDB = 'mongodb+srv://Eliana:bGUO2Rw31vqqY2Be@cluster0-pnyr8.mongodb.net/cafe';
-//}
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else{
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
