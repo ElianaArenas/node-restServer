7da32e14af07aaf5190254adc31c59e35d607523
Eliana Arenas <elianaarenasm@gmail.com>
2019-09-30 15:33:11 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,22 @@
 // Puerto
 // ==================================
 
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 3000;
 
+//===================================
+// Entorno
+// ==================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//===================================
+// Base de datos
+// ==================================
+let urlDB;
+
+//if(process.env.NODE_ENV === 'dev'){
+    //urlDB = 'mongodb://localhost:27017/cafe';
+//} else{
+    urlDB = 'mongodb+srv://Eliana:bGUO2Rw31vqqY2Be@cluster0-pnyr8.mongodb.net/cafe';
+//}
+
+process.env.URLDB = urlDB;
